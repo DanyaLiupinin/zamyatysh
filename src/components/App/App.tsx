abaa2d9156e03cf8058a08d40adc30c6076f3947
Danya Kovan <mr.kovan_2002@mail.ru>
2023-03-16 01:08:15 +0400

    init commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='test'>Замятыш</p>
+      <p className='testDva'>Zamyatysh</p>
+    </div>
+  );
+}
+
+export default App;
