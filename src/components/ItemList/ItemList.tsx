0c38decb3360c5ade3201c618e93b7aa2011cdae
Danya Kovan <mr.kovan_2002@mail.ru>
2023-03-20 20:20:45 +0400

    <ItemList /> desktop style basement added

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -3,7 +3,38 @@ import './ItemList.scss'
 const ItemList: React.FC = () => {
     return (
         <div className='itemList'>
+            <div className='itemList__container'>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
 
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+                <div className='itemList__item'>
+                    <p>dijiiio</p>
+                </div>
+
+            </div>
         </div>
     )
 }
