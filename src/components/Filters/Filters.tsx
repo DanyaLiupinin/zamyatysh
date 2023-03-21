4127ef8d60f53a3ec54590c35f9dca7e877a7bfc
Danya Kovan <mr.kovan_2002@mail.ru>
2023-03-21 11:15:40 +0400

    <Filters /> addaptive done

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -8,20 +8,40 @@ const Filters: React.FC<any> = ({ category, setCategory }: {
     category: String,
     setCategory: Dispatch<SetStateAction<boolean>>;
 }) => {
+
+    const categories = [
+        'все',
+        'футболки',
+        'худи',
+        'лонгсливы',
+        'аксессуары',
+        'стикеры',
+        'прочее'
+    ]
+
     return (
         <div className='filters'>
             <div className='filters__buttons'>
                 <div className='filters__dropdown'>
-                    <button type='button'>{category}</button>
-                    <img src={dropdown} alt='выбрать категорию'></img>
+                    {/* dropdown menu */}
+                    <form>
+                        <select onChange={(e) => console.log(e.target.value)}>
+                            {categories.map((category) => {
+                                return (
+                                    <option value={category} key={category}>{category}</option>
+                                )
+                            })}
+                        </select>
+                    </form>
                 </div>
-                <button className='filters__button' type='button'>все</button> {/* вынести категории в стейт, через map добавлять кнопки */}
-                <button className='filters__button' type='button'>футболки</button> {/* в выпадающем списке будет отсутствовать выбранная категория, так как она уже выбрана */}
-                <button className='filters__button' type='button'>худи</button> {/* подумать, как это лучше организовать */}
-                <button className='filters__button' type='button'>лонгсливы</button>
-                <button className='filters__button' type='button'>аксессуары</button>
-                <button className='filters__button' type='button'>стикеры</button>
-                <button className='filters__button' type='button'>прочий кал</button>
+                {/* desktop menu */}
+                {
+                    categories.map((category) => {
+                        return (
+                            <button className='filters__button' type='button' key={category}>{category}</button>
+                        )
+                    })
+                }
             </div>
         </div>
     )
