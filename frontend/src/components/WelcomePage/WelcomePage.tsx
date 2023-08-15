e931a9ae357d9ef9b1fda4b04759e3f431530a45
Danya Kovan <mr.kovan_2002@mail.ru>
2023-08-15 12:00:59 +0200

    feat: +locale in <WelcomePage /

--- frontend/src/components/WelcomePage/WelcomePage.tsx
+++ frontend/src/components/WelcomePage/WelcomePage.tsx
@@ -1,20 +1,21 @@
 import { Link } from 'react-router-dom'
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'   
 
 import Languages from '../Languages/Languages'
+
+import content from './locale.json'
+
+import { TLocale } from '../../types/components';
 
 import './WelcomePage.scss'
 
 const logo = require('../../images/gifka.GIF')
 
+
 const WelcomePage: React.FC = () => {
 
-    const locale = useSelector((state: any) => state.items.locale)
-
-    useEffect(() => {
-console.log(locale)
-    }, [locale])
+    const locale: TLocale = useSelector((state: any) => state.items.locale)
 
     return (
         <>
@@ -22,8 +23,8 @@ console.log(locale)
             <div className='welcome__container'>
                 <Languages />
                 <div className='welcome__links'>
-                    <Link to='/about' className='welcome__link'>о нас</Link>
-                    <Link to='/shop' className='welcome__link'>в магазин</Link>
+                    <Link to='/about' className='welcome__link'>{content.about[locale]}</Link>
+                    <Link to='/shop' className='welcome__link'>{content.shop[locale]}</Link>
                 </div>
                 <img className='welcome__gif' alt='лягушка-логотип' src={logo}></img>
             </div>
