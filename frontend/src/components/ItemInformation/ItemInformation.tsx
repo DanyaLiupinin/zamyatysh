5e2f5bfa598bea69a10439e3b2831fad80befcc5
Danya Kovan <mr.kovan_2002@mail.ru>
2023-08-24 19:43:01 +0200

    feat: + locale added to <ItemInformation />

--- frontend/src/components/ItemInformation/ItemInformation.tsx
+++ frontend/src/components/ItemInformation/ItemInformation.tsx
@@ -1,8 +1,15 @@
 import './ItemInformation.scss';
 
+import { useSelector } from 'react-redux';
+
 import frog from '../../images/vkorzini.svg';
+import { TLocale } from '../../types/components';
+
+import content from './locale.json';
 
 const ItemInformation = ({ item, chosenSize, setChosenSize }: any) => {
+
+    const locale: TLocale = useSelector((state: any) => state.items.locale);
 
     const onSizeClick = (size: string) => {
         if (chosenSize.includes(size)) {
@@ -28,11 +35,11 @@ const ItemInformation = ({ item, chosenSize, setChosenSize }: any) => {
                 }
             </div>
 
-            <p className='itemInformation__size-guide'>Как выбрать размер?</p>
+            <p className='itemInformation__size-guide'>{content.howToSize[locale]}</p>
 
             <div className='itemInformation__basket-container'> 
                 <img src={frog} className={`itemInformation__basket-image ${chosenSize !== '' ? 'itemInformation__basket-image_active' : ''}`} alt='frog wants you to buy something'></img>
-                <button className='itemInformation__add-button' type='button'>В корзину</button>
+                <button className='itemInformation__add-button' type='button'>{content.addToBasket[locale]}</button>
             </div>
         </div>
     );
