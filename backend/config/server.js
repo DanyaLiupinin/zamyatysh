8f1952c3f443050a8e48947016b25a61ba4f4f18
Danya Kovan <mr.kovan_2002@mail.ru>
2023-09-20 20:27:03 +0200

    host added

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   //host: env('HOST', '0.0.0.0'),
-  //port: env.int('PORT', 1337),
+  port: env.int('PORT', 1337),
   proxy: true,
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
