a82d410603674402accdcfff1e678f3ea1eb57cb
Danya Kovan <mr.kovan_2002@mail.ru>
2023-09-20 21:48:19 +0200

    feat: cors added

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,25 +1,39 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: "strapi::cors",
+    config: {
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "User-Agent",
+        "Origin",
+        "X-Requested-With",
+        "Accept",
+      ],
+      origin: ["*", "http://localhost:3000", "http://localhost:1337"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
