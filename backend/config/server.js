bf23d8b88f080b682517a52bf761a956d2841700
Danya Kovan <mr.kovan_2002@mail.ru>
2023-09-20 19:24:33 +0200

    changes for deploy

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  proxy: true,
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: 'https://zamyatysh-backend.onrender.com'
+  url: env('HOST_URL'),
+  app: {
+    keys: env.array('APP_KEYS')
+  },
 });
