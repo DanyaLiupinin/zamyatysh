daf3906f9ab919cb02d44000ae79b7221b993f1d
Danya Kovan <mr.kovan_2002@mail.ru>
2023-09-27 23:58:03 +0200

    feat: HashRouter added

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
