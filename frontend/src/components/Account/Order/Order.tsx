efb04f39b1d04d9748f877d7d71dd29982539ff7
DANILA KOVAN <pl2023289@365.stucom.com>
2023-11-17 09:44:31 +0100

    feat: layour for details of <Order />

--- frontend/src/components/Account/Order/Order.tsx
+++ frontend/src/components/Account/Order/Order.tsx
@@ -1,25 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Order.scss";
 
-import dropdown from '../../../images/dropdown.svg';
+import dropdown from "../../../images/dropdown.svg";
 
 const Order = () => {
-
     const [isOrderOpened, setOrderOpened] = useState(false);
+
+    // DOBAVIT PLASHKU CHTO VSIO OPLACHENO USPESHNO
 
     return (
         <>
             <div className='order'>
                 <div className='order__card'>
                     <p className='order__title'>order #12345</p>
-                    <button className='order__details-btn' onClick={() => setOrderOpened(!isOrderOpened)}>
+                    <button
+                        className='order__details-btn'
+                        onClick={() => setOrderOpened(!isOrderOpened)}
+                    >
                         <img src={dropdown} alt='open order details' />
                     </button>
                 </div>
 
-                {isOrderOpened && 
-                <div className='order-details' >
-                </div>}
+                {isOrderOpened && (
+                    <div className='order-details'>
+
+                        <div className='order-details__info'>
+                            <p className='order-details__date'>20.09.2023</p>
+                            <p className='order-details__status'>oplacheno</p>
+                        </div>
+                        <div className='order-details__items'>
+                            <div className='order-item'>
+                                <p>tshirt nomer 55</p>
+                                <p>size: m</p>
+                                <p>5000 kwaks</p>
+                            </div>
+                            <div className='order-item'>
+                                <p>tshirt nomer 55</p>
+                                <p>size: m</p>
+                                <p>5000 kwaks</p>
+                            </div>
+                            <div className='order-item'>
+                                <p>tshirt nomer 55</p>
+                                <p>size: m</p>
+                                <p>5000 kwaks</p>
+                            </div>
+                        </div>
+                        <p className='order-details__price'>total: 200000 kwaks</p>
+                    </div>
+                )}
             </div>
         </>
     );
