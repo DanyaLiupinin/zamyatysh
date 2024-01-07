9ed7c6c9a5a46ecb52ea0a4c2cea83b1ed14b1c7
Danya Kovan <mr.kovan_2002@mail.ru>
2024-01-07 17:15:40 +0100

    fix(Item visibility): <Item /> visibility changed according to new backend

--- frontend-only/src/components/Item/ItemInformation/ItemInformation.tsx
+++ frontend-only/src/components/Item/ItemInformation/ItemInformation.tsx
@@ -1,75 +1,26 @@
-import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import './ItemInformation.scss';
-
-import { TLocale } from '../../../types/components';
-import frog from '../../../images/vkorzini.svg';
-
 import content from '../../../locale/ItemInformation.json';
+import { IItem } from '../../../types/types';
+import { TLocale } from '../../../types/components';
 
-const ItemInformation = ({ 
-    item, 
-    chosenSize, 
-    setChosenSize, 
-    addItemHandler, 
-    isBasketItem, 
-    basketItemCount 
-}: any) => {
 
-    const [isCaptionActive, setCaptionActive] = useState(false);
-    
+export const ItemInformation = ({ item }: { item: IItem }) => {
+
     const locale: TLocale = useSelector((state: any) => state.items.locale);
 
-    const onSizeClick = (size: string) => {
-        if (chosenSize.includes(size)) {
-            setChosenSize('');
-        } else {
-            setChosenSize(size);
-            setCaptionActive(false);
-        }
-    };
-
-    const onAddItem = () => {
-        if (chosenSize === '') {
-            setCaptionActive(true);
-        } else {
-            addItemHandler();
-        }
-    };
-
     return (
-        <div className='itemInformation'>
-            <h2 className='itemInformation__title'>{item.title[locale]}</h2>
-            <p className='itemInformation__price'>{item.price} kwaks</p>
-            <div className='itemInformation__sizes'>
-                {
-                    item.sizes && item.sizes && item.sizes.map((size: string, index: number) => {
-                        return (
-                            <div onClick={() => onSizeClick(size.toUpperCase())} key={index} className={`itemInformation__size ${chosenSize === size.toUpperCase() ? 'itemInformation__size_active' : ''}`}>
-                                <p>{size.toUpperCase()}</p>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-            <p className='itemInformation__size-guide'>{content.howToSize[locale]}</p>
-            <div className='itemInformation__basket-container'>
-                <img src={frog} className={`itemInformation__basket-image ${chosenSize !== '' ? 'itemInformation__basket-image_active' : ''}`} alt='frog wants you to buy something'></img>
-                <button onClick={onAddItem} disabled={isCaptionActive ? true : false} className={`itemInformation__add-button`} type='button'>
-                    {
-                        content.addToBasket[locale]
-                    }
-                </button>
-                {isCaptionActive ?
-                    <p className='itemInformation__caption'>choose size please c:</p> : ''
-                }
-                {isBasketItem ? 
-                    <p className='itemInformation__caption'>you've already added {basketItemCount} c:</p> : '' 
-            }
-            </div>
+        <div id='description' className='itemCard__item-description'>
+            <h3>{content.description[locale]}:</h3>
+            <p>{item.description[locale]}</p>
+            <h3 className='itemCard__item-description-inline'>
+                {content.material[locale]}:{" "}
+            </h3>
+            <p>{item.material[locale]}</p>
+            <h3 className='itemCard__item-description-inline'>
+                {content.style[locale]}:
+            </h3>
+            <p>{item.style[locale]}</p>
         </div>
     );
 };
-
-export default ItemInformation;
--- frontend-only/src/pages/Item.tsx
+++ frontend-only/src/pages/Item.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./styles/Item/Item.scss";
@@ -8,34 +8,24 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ImageCarousel from "../components/Item/ImageCarousel/ImageCarousel";
 import BasketNotification from "../components/BasketNotification/BasketNotification";
-import ItemInformation from "../components/Item/ItemInformation/ItemInformation";
-import { TLocale } from "../types/components";
-import { getItem } from "../utils/api";
+import ItemMenu from "../components/Item/ItemMenu/ItemMenu";
 import { itemsActions } from "../store/items/index";
 import { useActionCreators } from "../store";
-
+import { ItemInformation } from "../components/Item/ItemInformation/ItemInformation";
 import { NavButton } from "../components/NavButton/NavButton";
-
 import { items } from "../constants/constants";
-
-import array from "../images/historyArray.svg";
-
 import { IItem } from "../types/types";
 
-import content from "../locale/ItemCard.json";
-
 const ItemCard: React.FC<any> = () => {
+
     const [item, setItem] = useState<any>();
-    const [chosenSize, setChosenSize] = useState<string>("");
+    const [chosenSize, setChosenSize] = useState<string>('');
     const [isBasketItem, setBasketItem] = useState(false);
     const [basketItemCount, setBasketItemCount] = useState(0);
 
-    const locale: TLocale = useSelector((state: any) => state.items.locale);
     const basketItems = useSelector((state: any) => state.items.basketItemsShort);
 
     const { setBasket } = useActionCreators(itemsActions);
-
-    const navigate = useNavigate();
 
     const param = useParams();
 
@@ -54,66 +44,66 @@ const ItemCard: React.FC<any> = () => {
         }
     };
 
+    // add basket counter to basket items
     useEffect(() => {
-        if (basketItems && item) {
+        const basketItemsHandler = () => {
             let itemCounter = 0;
             for (let i = 0; i < basketItems.length; i++) {
-                if (basketItems[i].slug === item.slug) {
+                if (Number(basketItems[i].id) === item.id) {
+                    console.log('da')
                     setBasketItem(true);
-                    itemCounter = ++ itemCounter;
+                    itemCounter = ++itemCounter;
                 }
             }
-
             setBasketItemCount(itemCounter);
+        };
+        if (basketItems && item) {
+            basketItemsHandler();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [basketItems, item]);
 
     useEffect(() => {
-        const actualItem = items.find((i: IItem) => {
-            return i.id === Number(param.id);
-        })
+        const getActualItem = () => {
+            const actualItem = items.find((i: IItem) => {
+                return i.id === Number(param.id);
+            });
 
-        setItem(actualItem);
+            if (actualItem)
+                setItem(actualItem);
+        };
+        getActualItem();
     }, []);
-    
+
     return (
         <>
-            <Header /> 
+            <Header />
             <section className='itemCard'>
                 <NavButton />
-                {item ? (
-                    <>
-                        <div className='itemCard__card-container'>
-                            <div className='itemCard__sticky-notification-container'>
-                                <BasketNotification />
+                {
+                    item ?
+                        <>
+                            <div className='itemCard__card-container'>
+                                <div className='itemCard__sticky-notification-container'>
+                                    <BasketNotification />
+                                </div>
+                                <ImageCarousel item={item} />
+                                <ItemMenu
+                                    chosenSize={chosenSize}
+                                    setChosenSize={setChosenSize}
+                                    item={item}
+                                    addItemHandler={addItemHandler}
+                                    isBasketItem={isBasketItem}
+                                    basketItemCount={basketItemCount}
+                                />
                             </div>
-                            <ImageCarousel item={item} />
                             <ItemInformation
-                                chosenSize={chosenSize}
-                                setChosenSize={setChosenSize}
                                 item={item}
-                                addItemHandler={addItemHandler}
-                                isBasketItem={isBasketItem}
-                                basketItemCount={basketItemCount}
                             />
-                        </div>
-                        <div id='description' className='itemCard__item-description'>
-                            <h3>{content.description[locale]}:</h3>
-                            <p>{item.description[locale]}</p>
-                            <h3 className='itemCard__item-description-inline'>
-                                {content.material[locale]}:{" "}
-                            </h3>
-                            <p>{item.material[locale]}</p>
-                            <h3 className='itemCard__item-description-inline'>
-                                {content.style[locale]}:
-                            </h3>
-                            <p>{item.style[locale]}</p>
-                        </div>
-                    </>
-                ) : (
-                    <p>oooops</p>
-                )}
+                        </>
+                        :
+                        <p className='itemCard__empty'>oooops, we couldnt find this item</p>
+                }
             </section>
             <Footer />
         </>
