f463501d4d2ed34b54fb07ddbc3dfee86358f2f5
DANILA KOVAN <pl2023289@365.stucom.com>
2024-02-09 08:30:50 +0100

    feat(Filters): <Filter /> components put to shared/components

---
+++ frontend-only/src/shared/ui/components/DesktopMenu/ui/DesktopMenu.tsx
@@ -0,0 +1,28 @@
+import './DesktopMenu.scss';
+import { useSelector } from 'react-redux';
+
+import { TLocale } from '../../../../../types/components';
+import { ICategory } from '../../../../../types/types';
+
+export const DesktopMenu = ({ categories, onClickFilter }:
+    {
+        categories: ICategory[],
+        onClickFilter: (category: string) => void,
+    }
+) => {
+
+    const activeCategoryFilter = useSelector((state: any) => state.items.activeCategoryFilter);
+    const locale: TLocale = useSelector((state: any) => state.items.locale);
+
+    return (
+        <div className='desktopMenu'>
+            {
+                categories && categories.map((category: any) => {
+                    return (
+                        <button className={`filters__button ${activeCategoryFilter === category && 'filters__button_active'}`} onClick={() => onClickFilter(category.title[locale])} type='button' key={category.id}>{`${category.title[locale]}`}</button>
+                    );
+                })
+            }
+        </div>
+    );
+};
