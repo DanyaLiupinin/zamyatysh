f0b7e9a1ec0710cc088571cfb263424f7746b181
Danya Kovan <mr.kovan_2002@mail.ru>
2024-02-18 22:53:01 +0100

    feat(about): text added to <About />

--- frontend-only/src/pages/About/ui/About.tsx
+++ frontend-only/src/pages/About/ui/About.tsx
@@ -1,12 +1,20 @@
+import { useSelector } from 'react-redux';
 import { Header } from '@widgets';
 import image from '@images/aboutUs.png';
+import content from '@locale/About.json';
+import { TLanguage } from '@types';
+
+import { selectLocale } from '../model/selectors';
 import './About.scss';
 
 export const About: React.FC = () => {
+    
+    const locale: TLanguage = useSelector(selectLocale);
     return (
         <>
             <div className='about'>
                 <Header />
+                <div><p>{content.about[locale]}</p></div>
                 <div className='about__image-container'>
                     <img className='about__image' src={image} alt='frogs'></img>
                 </div>
