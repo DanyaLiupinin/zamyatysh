08a2037b3aaab82ea12d89f19233146d3ee095ae
Danya Kovan <mr.kovan_2002@mail.ru>
2024-02-19 17:37:43 +0100

    feat(order body): locales added to <OrderBody />

--- frontend-only/src/features/Order/ui/OrderBody/ui/OrderBody.tsx
+++ frontend-only/src/features/Order/ui/OrderBody/ui/OrderBody.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
+import { useSelector } from "react-redux";
+import { TLanguage } from "@types";
 import { OrderBodyProps } from "features/Order/model/types/types";
+import content from '@locale/Order.json';
+
+import { selectLanguage } from '../../../model/selectors/selectors';
 import './OrderBody.scss';
 
-export const OrderBody: FC<OrderBodyProps> = ({date, price = 0, children}) => {
+export const OrderBody: FC<OrderBodyProps> = ({ date, price = 0, children }) => {
+    const locale: TLanguage = useSelector(selectLanguage);
     return (
         <div className='orderBody'>
             <div className='orderBody__info'>
                 <p className='orderBody__date'>{date}</p>
-                <p className='orderBody__status'>paid</p>
+                <p className='orderBody__status'>{content.paid[locale]}</p>
             </div>
             {children}
-            <p className='orderBody__price'>total: {price}</p>
+            <p className='orderBody__price'>{content.total[locale]}: {price}</p>
         </div>
     );
 };
