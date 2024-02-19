5f8d07a25761490191a4b4c02621b85d2d53086e
Danya Kovan <mr.kovan_2002@mail.ru>
2024-02-19 17:39:28 +0100

    feat(nav button) types added to <NavButton />

--- frontend-only/src/features/NavButton/ui/NavButton.tsx
+++ frontend-only/src/features/NavButton/ui/NavButton.tsx
@@ -1,17 +1,15 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { TLanguage } from "@types";
+import content from '@locale/NavButton.json';
 import array from '@images/historyArray.svg';
 
-import content from '@locale/NavButton.json';
-
+import { selectLanguage } from '../model/selectors/selectors';
 import './NavButton.scss';
 
 export const NavButton = () => {
-
     const navigate = useNavigate();
-    const locale: TLanguage = useSelector((state: any) => state.items.locale);
-
+    const locale: TLanguage = useSelector(selectLanguage);
     return (
         <button className='itemCard__navigation' onClick={() => navigate(-1)}>
             <img
